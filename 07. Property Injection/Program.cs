89fb1a78e9b0f52b323916da1a0607eca50286f5
unknown <nik@tumpedOver.com>
2014-09-19 05:55:46 -0500

    added property injection example

---
+++ 07. Property Injection/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+
+
+namespace CastleExamples
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var container = new WindsorContainer();
+            container.Register(Classes.FromThisAssembly().BasedOn<IRequireWindsor>().WithServiceAllInterfaces());
+
+            var world = container.Resolve<IFantasyWorld>();
+
+            Console.WriteLine("The center of the fantasy world is located at: \n{0}", world.WorldOrigin);
+
+            container.Dispose();
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface IFantasyWorld : IRequireWindsor
+    {
+        string WorldOrigin { get; }
+    }
+
+    public class FantasyWorld : IFantasyWorld
+    {
+        public IMagicalForest Forest { get; set; }
+
+        public string WorldOrigin
+        {
+            get { return Forest.Coordinates; }
+        }
+    }
+
+    public interface IMagicalForest : IRequireWindsor
+    {
+        string Coordinates { get; }
+    }
+
+    public class MagicalForest : IMagicalForest
+    {
+        public string Coordinates
+        {
+            get { return "Latitude : 33.096549 | Longitude : -96.67347"; }
+        }
+    }
+
+    public interface IRequireWindsor
+    {
+    }
+
+}
+
